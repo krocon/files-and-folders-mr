c5f212f1bcc5b63a947a67f8decc53c260806090
krocon@gmail.com <xPQxsnNVLNcfbRFGngY2>
2025-07-31 14:31:42 +0200

    refactor(environment): update API prefix to dynamically derive host and port configuration

--- apps/fnf/src/environments/environment.ts
+++ apps/fnf/src/environments/environment.ts
@@ -2,8 +2,10 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file-content replacements can be found in `angular.json`.
 
-const prefix = 'http://localhost:3333';
-const apiPrefix = prefix + "/api";
+const apiPort1 = 3333;
+const prefix1 = `${location.protocol}//${location.hostname}:${apiPort1}`;
+
+const apiPrefix1 = prefix1 + "/api";
 
 export const environment = {
   production: false,
@@ -11,83 +13,83 @@ export const environment = {
   commitHash: '70b27de',
 
   config: {
-    apiUrl: apiPrefix + "/config"
+    apiUrl: apiPrefix1 + "/config"
   },
   sysinfo: {
-    getDrivesUrl: apiPrefix + "/drives",
-    getSysinfoUrl: apiPrefix + "/sysinfo",
-    getAllinfoUrl: apiPrefix + "/allinfo",
-    getFirstStartFolderUrl: apiPrefix + "/firststartfolder"
+    getDrivesUrl: apiPrefix1 + "/drives",
+    getSysinfoUrl: apiPrefix1 + "/sysinfo",
+    getAllinfoUrl: apiPrefix1 + "/allinfo",
+    getFirstStartFolderUrl: apiPrefix1 + "/firststartfolder"
   },
   fileSystem: {
-    checkPathUrl: apiPrefix + "/checkpath",
-    filterExistsUrl: apiPrefix + "/filterexists",
-    readDirUrl: apiPrefix + "/readdir",
-    getfileattributesUrl: apiPrefix + "/getfileattributes",
-    setfileattributesUrl: apiPrefix + "/setfileattributes",
+    checkPathUrl: apiPrefix1 + "/checkpath",
+    filterExistsUrl: apiPrefix1 + "/filterexists",
+    readDirUrl: apiPrefix1 + "/readdir",
+    getfileattributesUrl: apiPrefix1 + "/getfileattributes",
+    setfileattributesUrl: apiPrefix1 + "/setfileattributes",
     defaultRoot: "/",
     fileWatcher: false
   },
   multiRename: {
-    convertnamesUrl: apiPrefix + "/ai/convertnames",
-    groupfilesUrl: apiPrefix + "/ai/groupfiles",
-    hasOpenAiApiKeyUrl: apiPrefix + "/ai/hasopenaiapikey"
+    convertnamesUrl: apiPrefix1 + "/ai/convertnames",
+    groupfilesUrl: apiPrefix1 + "/ai/groupfiles",
+    hasOpenAiApiKeyUrl: apiPrefix1 + "/ai/hasopenaiapikey"
   },
   checkGlob: {
-    apiUrl: apiPrefix + "/checkglob"
+    apiUrl: apiPrefix1 + "/checkglob"
   },
   clean: {
-    apiUrl: apiPrefix + "/clean"
+    apiUrl: apiPrefix1 + "/clean"
   },
   shell: {
-    shellUrl: apiPrefix + "/shell",
-    spawnUrl: apiPrefix + "/spawn",
-    cancelSpawnUrl: apiPrefix + "/cancelspawn",
+    shellUrl: apiPrefix1 + "/shell",
+    spawnUrl: apiPrefix1 + "/spawn",
+    cancelSpawnUrl: apiPrefix1 + "/cancelspawn",
   },
   walkdir: {
-    apiUrl: apiPrefix + "/walkdirsync",
+    apiUrl: apiPrefix1 + "/walkdirsync",
     syncMode: true
   },
   shellAutocomplete: {
-    autocompleteUrl: apiPrefix + "/shell-autocomplete"
+    autocompleteUrl: apiPrefix1 + "/shell-autocomplete"
   },
 
   configThemes: {
-    apiUrl: apiPrefix + "/themes"
+    apiUrl: apiPrefix1 + "/themes"
   },
 
   shortcut: {
-    apiUrl: apiPrefix + "/shortcuts"
+    apiUrl: apiPrefix1 + "/shortcuts"
   },
   filetypeExtensions: {
-    apiUrl: apiPrefix + "/filetypes"
+    apiUrl: apiPrefix1 + "/filetypes"
   },
   buttons: {
-    apiUrl: apiPrefix + "/buttons"
+    apiUrl: apiPrefix1 + "/buttons"
   },
   searchPatterns: {
-    apiUrl: apiPrefix + "/searchpatterns"
+    apiUrl: apiPrefix1 + "/searchpatterns"
   },
   prompt: {
-    apiUrl: apiPrefix + "/prompts"
+    apiUrl: apiPrefix1 + "/prompts"
   },
   edit: {
-    apiUrl: apiPrefix + "/file?name=",
-    saveFile: apiPrefix + "/file?name="
+    apiUrl: apiPrefix1 + "/file?name=",
+    saveFile: apiPrefix1 + "/file?name="
   },
   fileAction: {
-    url: apiPrefix + "/do",
-    multiUrl: apiPrefix + "/do/multi",
+    url: apiPrefix1 + "/do",
+    multiUrl: apiPrefix1 + "/do/multi",
   },
   gotoAnything: {
-    apiUrl: apiPrefix + "/findfolders"
+    apiUrl: apiPrefix1 + "/findfolders"
   },
   tool: {
-    loadUrl: apiPrefix + "/tools",
-    shellUrl: apiPrefix + "/shell"
+    loadUrl: apiPrefix1 + "/tools",
+    shellUrl: apiPrefix1 + "/shell"
   },
   download: {
-    downloadUrl: apiPrefix + "/download"
+    downloadUrl: apiPrefix1 + "/download"
   }
 };
 
